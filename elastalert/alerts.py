47037e8dbef6156d9bb126be18c8159d459f5955
ranjithruban <ranjith.ruban@auto1.com>
2017-09-28 15:00:46 +0200

    Add resolve for email

--- elastalert/alerts.py
+++ elastalert/alerts.py
@@ -380,6 +380,7 @@ class EmailAlerter(Alerter):
             self.get_account(self.rule['smtp_auth_file'])
         self.smtp_key_file = self.rule.get('smtp_key_file')
         self.smtp_cert_file = self.rule.get('smtp_cert_file')
+        self.email_resolve_alert = self.rule.get('resolve_alert', None)
         # Convert email to a list if it isn't already
         if isinstance(self.rule['email'], basestring):
             self.rule['email'] = [self.rule['email']]
@@ -397,7 +398,6 @@ class EmailAlerter(Alerter):
 
     def alert(self, matches):
         body = self.create_alert_body(matches)
-
         # Add JIRA ticket if it exists
         if self.pipeline is not None and 'jira_ticket' in self.pipeline:
             url = '%s/browse/%s' % (self.pipeline['jira_server'], self.pipeline['jira_ticket'])
@@ -451,7 +451,65 @@ class EmailAlerter(Alerter):
         self.smtp.close()
 
         elastalert_logger.info("Sent email to %s" % (to_addr))
+    def resolve(self):
+	if self.email_resolve_alert == True:
+            body = 'Alert resolved'
+	    # Add JIRA ticket if it exists
+            if self.pipeline is not None and 'jira_ticket' in self.pipeline:
+                url = '%s/browse/%s' % (self.pipeline['jira_server'], self.pipeline['jira_ticket'])
+                body += '\nJIRA ticket: %s' % (url)
 
+            to_addr = self.rule['email']
+            if 'email_from_field' in self.rule:
+                recipient = lookup_es_key(matches[0], self.rule['email_from_field'])
+                if isinstance(recipient, basestring):
+                    if '@' in recipient:
+                        to_addr = [recipient]
+                    elif 'email_add_domain' in self.rule:
+                        to_addr = [recipient + self.rule['email_add_domain']]
+                elif isinstance(recipient, list):
+                    to_addr = recipient
+                    if 'email_add_domain' in self.rule:
+                        to_addr = [name + self.rule['email_add_domain'] for name in to_addr]
+            email_msg = MIMEText(body.encode('UTF-8'), _charset='UTF-8')
+            email_msg['Subject'] = self.create_title(matches)+'Resolved'
+            email_msg['To'] = ', '.join(to_addr)
+            email_msg['From'] = self.from_addr
+            email_msg['Reply-To'] = self.rule.get('email_reply_to', email_msg['To'])
+            email_msg['Date'] = formatdate()
+            if self.rule.get('cc'):
+                email_msg['CC'] = ','.join(self.rule['cc'])
+                to_addr = to_addr + self.rule['cc']
+            if self.rule.get('bcc'):
+                to_addr = to_addr + self.rule['bcc']
+
+            try:
+                if self.smtp_ssl:
+                    if self.smtp_port:
+                        self.smtp = SMTP_SSL(self.smtp_host, self.smtp_port, keyfile=self.smtp_key_file, certfile=self.smtp_cert_file)
+                    else:
+                        self.smtp = SMTP_SSL(self.smtp_host, keyfile=self.smtp_key_file, certfile=self.smtp_cert_file)
+                else:
+                    if self.smtp_port:
+                        self.smtp = SMTP(self.smtp_host, self.smtp_port)
+                    else:
+                        self.smtp = SMTP(self.smtp_host)
+                    self.smtp.ehlo()
+                    if self.smtp.has_extn('STARTTLS'):
+                        self.smtp.starttls(keyfile=self.smtp_key_file, certfile=self.smtp_cert_file)
+                if 'smtp_auth_file' in self.rule:
+                    self.smtp.login(self.user, self.password)
+            except (SMTPException, error) as e:
+                raise EAException("Error connecting to SMTP host: %s" % (e))
+            except SMTPAuthenticationError as e:
+                raise EAException("SMTP username/password rejected: %s" % (e))
+            self.smtp.sendmail(self.from_addr, to_addr, email_msg.as_string())
+            self.smtp.close()
+
+            elastalert_logger.info("Resolve email Sent to %s" % (to_addr))
+        else:
+            elastalert_logger.info("Alert not sent to Slack as resolve alert is False") 
+ 
     def create_default_title(self, matches):
         subject = 'ElastAlert: %s' % (self.rule['name'])
 
